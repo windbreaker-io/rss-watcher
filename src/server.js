25ad7893905b90aa077621dcb8efc5905142cd8b
Mabry Cervin <Mabry.Cervin@inin.com>
2017-06-26 21:47:09 -0400

    Added comments

--- src/server.js
+++ src/server.js
@@ -14,10 +14,17 @@ const produceDiff = function(acc, cur) {
     return cur
 }
 
+// Set up the node streams
 const req = request('https://pypi.python.org/pypi?%3aaction=rss')
 const feedparser = new FeedParser()
+
+// Convert the request stream into an Rx observable
 RxNode.fromStream(req, 'end', 'response')
+  // Map the events from the request Observable to an observable made from the FeedParser
   .flatMap(x => RxNode.fromReadableStream(x.pipe(feedparser)))
+  // Collect events from parsing the RSS feed into a single array
   .buffer(Rx.Observable.fromEvent(req, 'end'))
+  // Use scan to compare the most recent feed to the previous
   .scan(produceDiff, [])
+  // NOP subscribe for now to complete the chain
   .subscribe(x => x)
