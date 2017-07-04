0a5e2a30ba3339caf2dbf50be6a0969c4d471a3e
Mabry Cervin <Mabry.Cervin@inin.com>
2017-07-04 18:23:50 -0400

    Added logging file

---
+++ src/logging/index.js
@@ -0,0 +1,15 @@
+/**
+ * Exports a logger
+ */
+
+const logging = require('windbreaker-service-util/logging')
+const config = require('~/src/config')
+
+const packagePath = require.resolve('~/package.json')
+
+logging.configure({
+  packagePath,
+  logLevel: config.getLogLevel()
+})
+
+module.exports = logging
