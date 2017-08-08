4dd9074b32747abb94e49d719e39708c251b4687
Mabry Cervin <Mabry.Cervin@inin.com>
2017-08-08 17:53:49 -0400

    Removed extra file

--- test/unit/diff.js
+++ test/unit/diff.js
@@ -6,7 +6,7 @@ const rssDiff = require('~/src/rss-diff')
 
 const beforeFeed = require('./before_feed.json')
 const afterFeed = require('./after_feed.json')
-const feedDiff = require('./diff.json')
+const feedDiff = require('./feed_diff.json')
 
 test('test json diffing', t => {
 
