d07e0f72855755f5afcc45c36ef69d260906bd22
Mabry Cervin <Mabry.Cervin@inin.com>
2017-08-15 18:17:01 -0400

    [NO-ISSUE] Added eslint and commit-branch-modifier

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ config.load()
 const logger = require('~/src/logging').logger(module)
 const FeedParser = require('feedparser')
 const request = require('request')
-const Rx = require('rx') 
+const Rx = require('rx')
 const RxNode = require('rx-node')
 
 const rssDiff = require('~/src/rss-diff')
@@ -18,31 +18,31 @@ const POLLING_INTERVAL = config.getPollingInterval()
 // Set up an interval to poll the feed
 const interval = Rx.Observable.interval(POLLING_INTERVAL).timeInterval().take(300)
 
-const produceDiff = function(acc, cur) {
-    // Here for debugging purposes
-    if (!(JSON.stringify(cur) === JSON.stringify(acc))) {
-        logger.debug('!!!!!!!!!!!!!!!!!!!')
-        for (let i = 0; i < acc.length; i++) {
-            logger.debug(acc[i]['title'])
-        }
-        logger.debug('(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)')
-        for (let i = 0; i < cur.length; i++) {
-            logger.debug(cur[i]['title'])
-        }
-        logger.debug('+++++++++++++++++++')
+const produceDiff = function (acc, cur) {
+  // Here for debugging purposes
+  if (!(JSON.stringify(cur) === JSON.stringify(acc))) {
+    logger.debug('!!!!!!!!!!!!!!!!!!!')
+    for (let i = 0; i < acc.length; i++) {
+      logger.debug(acc[i]['title'])
     }
-    logger.debug(rssDiff(acc, cur, 'title'))
-    return cur
+    logger.debug('(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)')
+    for (let i = 0; i < cur.length; i++) {
+      logger.debug(cur[i]['title'])
+    }
+    logger.debug('+++++++++++++++++++')
+  }
+  logger.debug(rssDiff(acc, cur, 'title'))
+  return cur
 }
 
 // Take requests and convert to observables
-const requestObservable = function(req) {
-    return RxNode.fromStream(req, 'end', 'response')
+const requestObservable = function (req) {
+  return RxNode.fromStream(req, 'end', 'response')
 }
 
 // Take request observables, parse them as RSS, and return results in an array
-const feedObservable = function(res) {
-    return RxNode.fromReadableStream(res.pipe(new FeedParser)).toArray()
+const feedObservable = function (res) {
+  return RxNode.fromReadableStream(res.pipe(new FeedParser())).toArray()
 }
 
 // Generate a new request for each interval
