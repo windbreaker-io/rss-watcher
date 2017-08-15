574eb1ee8ccabdc12c77b8f80cd3b56ab1d27e4c
Mabry Cervin <Mabry.Cervin@inin.com>
2017-08-15 17:25:36 -0400

    Created new branch and fixed PR comments

--- test/unit/rss-diff/rss-diff-test.js
+++ test/unit/rss-diff/rss-diff-test.js
@@ -3,12 +3,11 @@ const test = require('ava')
 
 const rssDiff = require('~/src/rss-diff')
 
-const beforeFeed = require('./before_feed.json')
-const afterFeed = require('./after_feed.json')
-const feedDiff = require('./feed_diff.json')
+const beforeFeed = require('./fixtures/before_feed.json')
+const afterFeed = require('./fixtures/after_feed.json')
+const feedDiff = require('./fixtures/feed_diff.json')
 
 test('test json diffing', t => {
-
     const calcDiff = rssDiff(beforeFeed, afterFeed, 'title')
     t.deepEqual(calcDiff, feedDiff)
 })
