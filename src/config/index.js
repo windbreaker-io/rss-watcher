35b3817f04894562721b42ced074015b931a776f
Charlie Duong <charlieduong94@gmail.com>
2017-08-27 17:22:34 -0400

    Updated compose file to include server container Added missing dependencies
    Use new config loading api

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
-const registerConfig = require('windbreaker-service-util/config')
+const configUtil = require('windbreaker-service-util/config')
 
 const Config = require('./Config')
 const config = module.exports = new Config()
 
-registerConfig(config)
+module.exports.load = async () => {
+  await configUtil.load({ config })
+}
