60d666275072eb46b4b8cf7781e0856976ddc40e
Mabry Cervin <mabry.cervin@gmail.com>
2017-10-15 18:09:27 -0400

    [NO-ISSUE] WIP: squash

--- test/integration/rss-watcher.js
+++ test/integration/rss-watcher.js
@@ -1,6 +1,29 @@
+const { fork } = require('child_process')
+const nock = require('nock')
 const test = require('ava')
+const uuid = require('uuid/v4')
 
 const { createConsumer } = require('windbreaker-service-util/queue')
 
+const PYPI_URL = 'https://pypi.python.org/pypi?%3aaction=rss'
+
 test.beforeEach('setup queue consumer', async (t) => {
-});
+  t.context.messages = []
+  t.context.onMessage = msg => {
+    messages.push(msg)
+  }
+
+  t.context.queueName = uuid()
+  const rabbitConsumer = await createConsumer({
+    logger: console,
+    amqUrl: 'rabbitmq',
+    onMessage: t.context.onMessage,
+    consumerOptions: {queueName: t.context.queueName}
+  })
+})
+
+test('should push pypi changes to RabbitMQ', async (t) => {
+  const rssWatcherProc = fork('../../src/server.js', [], {
+    stdio: 'pipe'
+  })
+})
--- test/unit/rss-diff/rss-diff-test.js
+++ test/unit/rss-diff/rss-diff-test.js
@@ -3,9 +3,9 @@ const test = require('ava')
 
 const rssDiff = require('~/src/rss-diff')
 
-const beforeFeed = require('./fixtures/before_feed.json')
-const afterFeed = require('./fixtures/after_feed.json')
-const feedDiff = require('./fixtures/feed_diff.json')
+const beforeFeed = require('../../fixtures/before_feed.json')
+const afterFeed = require('../../fixtures/after_feed.json')
+const feedDiff = require('../../fixtures/feed_diff.json')
 
 test('test json diffing', t => {
     const calcDiff = rssDiff(beforeFeed, afterFeed, 'title')
